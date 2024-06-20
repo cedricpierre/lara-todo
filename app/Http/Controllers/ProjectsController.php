9484379aaf3e643578b28d393f2dda9eb8aa8188
Cédric Pierre <cedric.pierre@freecaster.com>
2024-06-20 11:27:22 +0200

    Fix sail

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateProjectRequest;
+use App\Http\Requests\CreateTodoRequest;
 use App\Http\Resources\ProjectResource;
+use App\Http\Resources\TodoResource;
 use App\Models\Project;
+use App\Models\Todo;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ProjectsController extends Controller
@@ -15,4 +19,15 @@ class ProjectsController extends Controller
     {
         return ProjectResource::collection(Project::all());
     }
+
+    /**
+     * @param CreateProjectRequest $request
+     * @return ProjectResource
+     */
+    public function store(CreateProjectRequest $request): ProjectResource
+    {
+        $project = Project::query()->create($request->all());
+        return new ProjectResource($project);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('project', [ProjectsController::class, 'index'])->name('project.index');
+Route::get('projects', [ProjectsController::class, 'index'])->name('project.index');
+Route::post('projects', [ProjectsController::class, 'store'])->name('project.store');
 
 Route::get('todos', [TodosController::class, 'index'])->name('todos.index');
+Route::post('todos', [TodosController::class, 'store'])->name('todos.store');
 Route::get('todos/{id}', [TodosController::class, 'show'])->name('todos.show');
-Route::post('todos/{id}', [TodosController::class, 'store'])->name('todos.store');
 Route::patch('todos/{id}', [TodosController::class, 'update'])->name('todos.update');
 Route::delete('todos/{id}', [TodosController::class, 'destroy'])->name('todos.destroy');
 
